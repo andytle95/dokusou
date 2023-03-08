fabedd19ee5f99b1031f748916771a3aa1495a97
Andy Le <andytle95@gmail.com>
2023-03-08 09:50:31 -0800

    website started

---
+++ dokusou/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './container/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Dokusou</h1> */}
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ dokusou/src/container/Navbar.js
@@ -0,0 +1,9 @@
+import './Navbar.css'
+
+let Navbar = () => {
+    return (
+    <h1>Gregg is gay</h1>
+    );
+}
+
+export default Navbar;
