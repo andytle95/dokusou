b80ccf41ab4d5ee9fcee2fdc88aaba0e8dccb9de
Andy Le <andytle95@gmail.com>
2023-03-30 11:54:29 -0700

    back on grind

--- dokusou/src/App.js
+++ dokusou/src/App.js
@@ -2,7 +2,7 @@ import Navbar from './container/Navbar';
 // import Main from './container/Main'
 import './App.css';
 //main page.
-
+// back on grind
 function App() {
   return (
     <div className="App">
--- dokusou/src/container/Navbar.js
+++ dokusou/src/container/Navbar.js
@@ -1,34 +1,93 @@
+
+
+// v2 code VVVVVVV
+
+
 import React from 'react';
-import './Navbar.css'
-import './../App.css'
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './Navbar.css';
 
-//need to make functionality where when the text logo is clicked,
-// we are brought back up to the top of the page.
+const Navbar = () => {
+    const [showMenu, setShowMenu] = React.useState(false);
 
-//first need to add a border and background to the logo.
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
-//need to find appropriate color for the navbar
-
-// anday
-
-let Navbar = () => {
     return (
-        <nav className='navbar'>
-            <a class="logo" href="#">THE DOKUSOU CAST</a>
-            {/* <ul>
+        <nav className={`navbar ${showMenu ? 'active' : ''}`}>
+            <div className="logo">
+                <Link to="top" smooth={true}>
+                    THE DOKUSOU CAST
+                </Link>
+            </div>
+            <ul className={`nav-links ${showMenu ? 'active' : ''}`}>
                 <li>
-                    <a href='#'>Home</a>
+                    <Link to="about" smooth={true} onClick={toggleMenu}>
+                        About
+                    </Link>
                 </li>
                 <li>
-                    <a href='#'>About</a>
+                    <Link to="contact" smooth={true} onClick={toggleMenu}>
+                        Contact
+                    </Link>
                 </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul> */}
-            {/* <div>Logo</div> */}
+            </ul>
+            <div className="menu-icon" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+// VVVVVVV v1 code VVVVVV
+
+
+// import React from 'react';
+// import './Navbar.css'
+// import './../App.css'
+
+// //need to make functionality where when the text logo is clicked,
+// // we are brought back up to the top of the page.
+
+// //first need to add a border and background to the logo.
+
+// //need to find appropriate color for the navbar
+
+// // anday
+
+// let Navbar = () => {
+//     return (
+//         <nav className='navbar'>
+//             <a class="logo" href="#">THE DOKUSOU CAST</a>
+//             {/* <ul>
+//                 <li>
+//                     <a href='#'>Home</a>
+//                 </li>
+//                 <li>
+//                     <a href='#'>About</a>
+//                 </li>
+//                 <li>
+//                     <a href="#">Contact</a>
+//                 </li>
+//             </ul> */}
+//             {/* <div>Logo</div> */}
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
