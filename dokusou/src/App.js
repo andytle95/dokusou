2ba1fdf0f89d583e62ee66e33660f6e95acd8683
Andy Le <andytle95@gmail.com>
2023-04-13 17:07:50 -0700

    about me color changed

--- dokusou/src/App.js
+++ dokusou/src/App.js
@@ -1,7 +1,7 @@
 // import Navbar from './container/Navbar';
 import Main from './container/Main.js'
 import AboutMe from './container/AboutMe.js'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       {/* nav class that shows logo style and links to scroll down to */}
       <nav className='nav'>.
-      <a href="#" className='nav-item'>Homepage</a>
-      <a href="#" className='nav-item'>About Us</a>
+      <Link to="#" className='nav-item'>Homepage</Link>
+      <Link to="#" className='nav-item'>About Us</Link>
       </nav>
       {/* Routes in order to scroll through the page. */}
       <Routes>
